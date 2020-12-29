44945795952d53f653c56b5475198c03c3ede98a
Dhanushgeek <dhanushgeek@gmail.com>
2020-12-30 03:10:30 +0530

    js update

--- game.js
+++ game.js
@@ -17,19 +17,6 @@ $(".btn").click(function () {
 
 
 
-function nextSequence() {
-    var randomNumber = (Math.floor(Math.random() * 4));
-
-    var randomChosenColor = buttonColors[randomNumber];
-
-    gamePattern.push(randomChosenColor);
-
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    playSound(randomChosenColor);
-
-}
-
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
@@ -49,15 +36,45 @@ function animatePress(currentColor) {
 
 function checkAnswer(currrentLevel) {
     if (gamePattern[currrentLevel] === userClickedPattern[currrentLevel]) {
-    
 
 
-    if (userClickedPattern.length === gamePattern.length) {
-        setTimeout(function () {
-            nextSequence();
-        }, 1000);
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game over!! ,Press any key to restart");
+
     }
 
 }
+
+
+function nextSequence() {
+
+    userClickedPattern=[];
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = (Math.floor(Math.random() * 4));
+
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
 
 }
